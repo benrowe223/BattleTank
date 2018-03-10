1f7f2f1a0b2d4de61e411803fe6056566a46e9c7
ben.rowe <benrowe223@gmail.com>
2018-03-10 20:51:56 +0000

    Barrel pitch movement

--- Source/BattleTank/Private/TankBarrel.cpp
+++ Source/BattleTank/Private/TankBarrel.cpp
@@ -4,9 +4,13 @@
 #include "TankBarrel.h"
 
 
-void UTankBarrel::Elevate(float DegreesPerSecond)
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// Move the barrel the right amount this frame
 	// Given a max elevation speed, and the frame time
-	UE_LOG(LogTemp, Warning, TEXT("Barrel->Elevate() called at speed %f"), DegreesPerSecond);
+	auto ClampedRelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	auto Elevation = FMath::Clamp<float>(RawNewElevation, 0, 0);
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
